1b74f0a66fe1e62a89dffae62cf689ac1519652b
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-07-16 13:21:08 +0530

    Setup eslint and prettier.

---
+++ .prettierrc.js
@@ -0,0 +1,1 @@
+module.exports = { endOfLine: 'lf', jsxSingleQuote: true, printWidth: 120, singleQuote: true, trailingComma: 'all' };
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div></div>
-  );
+function App(): JSX.Element {
+  return <div />;
 }
 
 export default App;
